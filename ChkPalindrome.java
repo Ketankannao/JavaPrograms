5f8e6d36f593e48ad6080ac6af47df492ae19596
Payal <payalharne@yahoo.com>
2019-10-14 10:02:44 +0530

    no changes
    ar

--- ChkPalindrome.java
+++ ChkPalindrome.java
@@ -13,9 +13,10 @@ class ChkPalindrome
       int length = str.length();
  
       for ( int i = length - 1; i >= 0; i-- )
-         rev = rev + str.charAt(i);
+                rev = rev + str.charAt(i);
  
       if (str.equals(rev))
+         System.out.println(str+" is a palindrome");
          System.out.println(str+" is a palindrome");
       else
          System.out.println(str+" is not a palindrome");
